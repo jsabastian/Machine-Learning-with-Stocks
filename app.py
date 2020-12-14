7bf8c9a34779f7bb2c199562d39773309fae96b5
jsabastian <john.santoyo@email.com>
2020-12-13 23:07:16 -0600

    added dash app stuff

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from alpha_vantage.timeseries import TimeSeries
 # import os
 # Add variable from environments
 # load_dotenv()
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
